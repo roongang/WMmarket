2faed132f445f71aa7f127f7430bf6f1476c6bfb
shininghyunho <gusgh3315@uos.ac.kr>
2021-10-07 16:04:27 +0900

    Initial commit

---
+++ src/main/java/com/around/wmmarket/WMmarketApplication.java
@@ -0,0 +1,13 @@
+package com.around.wmmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WMmarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WMmarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/around/wmmarket/WMmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.around.wmmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WMmarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
