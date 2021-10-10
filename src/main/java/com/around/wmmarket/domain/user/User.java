e2c9ba8f710ba38bfc22f19080dd548262122aad
shininghyunho <gusgh3315@uos.ac.kr>
2021-10-10 21:32:57 +0900

    add user entity

---
+++ src/main/java/com/around/wmmarket/domain/user/User.java
@@ -0,0 +1,41 @@
+package com.around.wmmarket.domain.user;
+
+import com.around.wmmarket.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "user")
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String image;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public User(String email,String password,String image,String nickname){
+        this.email=email;
+        this.password=password;
+        this.image=image;
+        this.nickname=nickname;
+    }
+}
