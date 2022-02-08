70bf7395a2f072dd17615ef6a92ba07564beb78a
shininghyunho <gusgh3315@uos.ac.kr>
2022-02-08 13:31:52 +0900

    test

--- src/main/java/com/around/wmmarket/config/WebSocketConfig.java
+++ src/main/java/com/around/wmmarket/config/WebSocketConfig.java
@@ -11,11 +11,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
     }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/pub");
-        registry.enableSimpleBroker("/sub");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
     }
 }
---
+++ src/main/java/com/around/wmmarket/controller/dto/notification/Message.java
@@ -0,0 +1,11 @@
+package com.around.wmmarket.controller.dto.notification;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Message {
+    private String from;
+    private String message;
+}
